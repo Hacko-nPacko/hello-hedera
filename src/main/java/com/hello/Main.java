8902719cc3218483653427f97775c4eea221df9c
Atanas Balevsky <atanas.balevsky@gmail.com>
2022-09-13 00:05:06 +0300

    hedera starter

--- src/main/java/com/hello/Main.java
+++ src/main/java/com/hello/Main.java
@@ -1,10 +1,16 @@
 package com.hello;
 
-import org.apache.commons.lang3.StringUtils;
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        boolean b = StringUtils.equals("nas", "pras");
-        System.out.println("Hello world! " + b);
+        log.info("{}", Dotenv.load());
+        AccountId myAccountId = AccountId.fromString(Dotenv.load().get("MY_ACCOUNT_ID"));
+        PrivateKey myPrivateKey = PrivateKey.fromString(Dotenv.load().get("MY_PRIVATE_KEY"));
+        log.info("{} {}", myAccountId, myPrivateKey);
     }
 }
