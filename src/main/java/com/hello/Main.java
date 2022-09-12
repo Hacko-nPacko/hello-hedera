31f25800abab537869a6433cf76e555612b2c632
Atanas Balevsky <atanas.balevsky@gmail.com>
2022-09-12 23:49:59 +0300

    spring boot jar starter

--- src/main/java/com/hello/Main.java
+++ src/main/java/com/hello/Main.java
@@ -1,7 +1,10 @@
 package com.hello;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean b = StringUtils.equals("nas", "pras");
+        System.out.println("Hello world! " + b);
     }
 }
